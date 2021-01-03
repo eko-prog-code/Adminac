b88d102c96c05e04f5f711ac5031076a890b2165
eko-prog-code <ekosetiaji929@gmail.com>
2021-01-03 19:49:02 +0700

    apk staff Alo Care
    Alo Csre

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import Splash from './Splash';
+import GetStarted from './GetStarted';
+import Register from './Register';
+import Login from './Login';
+import UploadPhoto from './UploadPhoto';
+import Admin from './Admin';
+import Messages from './Messages';
+import Info from './Info';
+import ChooseDoctor from './ChooseDoctor';
+import Chatting from './Chatting';
+import UserProfile from './UserProfile';
+import UpdateProfile from './UpdateProfile';
+import DoctorProfile from './DoctorProfile';
+import Detail from './Detail';
+
+export {
+  Splash,
+  GetStarted,
+  Register,
+  Login,
+  UploadPhoto,
+  Admin,
+  Messages,
+  Info,
+  ChooseDoctor,
+  Chatting,
+  UserProfile,
+  UpdateProfile,
+  DoctorProfile,
+  Detail,
+};
